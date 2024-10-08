59511a5bea5f524baa85836a3bb389d6fff9e415
girdhari <girdharikumar101@gmail.com>
2024-10-08 22:27:40 +0530

    initial changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>We're Currently Under Maintenance</h1>
+        <p>We'll be back soon! Stay tuned.</p>
       </header>
     </div>
   );
