428914fb37fe2ca1924217c8619103790ba08969
Girdhari Thakur <girdharikumar101@gmail.com>
2025-02-21 22:44:00 +0530

    First Commit

---
+++ idn_backend/src/test/java/com/indiadefencenews/idn_backend/IdnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indiadefencenews.idn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
