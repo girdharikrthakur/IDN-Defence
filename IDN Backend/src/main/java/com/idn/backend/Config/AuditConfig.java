ebbf2ff8e159409ba99fb31ce37d0de9be338d77
Girdhari Thakur <girdharikumar101@gmail.com>
2026-03-24 01:12:09 +0530

    removed duplicate bean auditorProvider

--- IDN Backend/src/main/java/com/idn/backend/Config/AuditConfig.java
+++
@@ -1,9 +0,0 @@
-package com.idn.backend.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class AuditConfig {
-}
